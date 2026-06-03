2651bdc46a11b4737f297c226049fce62773b868
jc09170307 <joncenteno92@gmail.com>
2026-06-03 16:04:13 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'vuim-planner-v1';
+const ASSETS = [
+  '/vuim-planner/',
+  '/vuim-planner/index.html',
+  '/vuim-planner/manifest.json',
+  '/vuim-planner/icon-192.png',
+  '/vuim-planner/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(resp => {
+        if (!resp || resp.status !== 200 || resp.type !== 'basic') return resp;
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      }).catch(() => caches.match('/vuim-planner/index.html'));
+    })
+  );
+});
